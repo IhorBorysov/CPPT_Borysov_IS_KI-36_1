6686da58ec11eef66a43f04b015ff2150fb6f1f9
IhorBorysov <95441954+IhorBorysov@users.noreply.github.com>
2022-08-23 19:13:21 +0300

    LAB_01

---
+++ src/main/java/LAB_01/Main.java
@@ -0,0 +1,9 @@
+package LAB_01;
+
+public class Main {
+    public static void main(String[] args) {
+        String message;
+        message = "Borysov Ihor KI-36";
+        System.out.println(message);
+    }
+}
