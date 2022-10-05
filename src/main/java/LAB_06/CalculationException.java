1d9b2c8371875428298fae2cb955d1a8b15d12fe
IhorBorysov <95441954+IhorBorysov@users.noreply.github.com>
2022-10-05 12:43:16 +0300

    LAB_06

---
+++ src/main/java/LAB_06/CalculationException.java
@@ -0,0 +1,12 @@
+package LAB_06;
+
+public class CalculationException extends ArithmeticException {
+
+    public CalculationException() {
+        super();
+    }
+
+    public CalculationException(String cause) {
+        super(cause);
+    }
+}
